d327b4dbc0a649f5eb6427e4688fcbd0131749c5
zohaib <zohaibansari100@gmail.com>
2021-04-10 18:39:53 +0530

    add delete option

--- ComposeNotesDesktop/src/main/kotlin/ui/NoteDetails/NoteDetails.kt
+++ ComposeNotesDesktop/src/main/kotlin/ui/NoteDetails/NoteDetails.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,10 +18,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import theme.purpleD1
 import theme.purpleD3
+import theme.robotoCus
 
 @Composable
 fun NoteDetails(noteId: Int, onBack: () -> Unit) {
@@ -27,23 +31,48 @@ fun NoteDetails(noteId: Int, onBack: () -> Unit) {
     val noteQueries = DatabaseHelper.queries
     val note = noteQueries.selectNote(note_number = noteId.toLong()).executeAsOneOrNull()
 
-    var title = remember { mutableStateOf(note?.title) }
-    var body = remember { mutableStateOf(note?.body) }
+    val title = remember { mutableStateOf(note?.title) }
+    val body = remember { mutableStateOf(note?.body) }
 
     Column(
         modifier = Modifier.background(purpleD1)
             .fillMaxSize()
     ) {
-        Icon(
-            imageVector = Icons.Filled.ArrowBack,
-            tint = Color.White,
-            contentDescription = "back-button",
-            modifier = Modifier
-                .padding(start = 20.dp, top = 10.dp)
-                .clickable {
-                    noteQueries.updateNote(title.value.toString(), body.value.toString(), noteId.toLong())
-                    onBack.invoke()
-                })
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                tint = Color.White,
+                contentDescription = "back-button",
+                modifier = Modifier
+                    .padding(start = 20.dp, top = 15.dp)
+                    .clickable {
+                        noteQueries.updateNote(title.value.toString(), body.value.toString(), noteId.toLong())
+                        onBack.invoke()
+                    })
+            Text(
+                modifier = Modifier.padding(top = 10.dp),
+                text = "Edit Note",
+                style = robotoCus.caption,
+                fontSize = 25.sp,
+                color = Color.White,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+            Icon(
+                imageVector = Icons.Filled.Delete,
+                tint = Color.White,
+                contentDescription = "delete-button",
+                modifier = Modifier
+                    .padding(end = 20.dp, top = 15.dp)
+                    .clickable {
+                        noteQueries.deleteNote(noteId.toLong())
+                        onBack.invoke()
+                    })
+        }
 
         TextField(
             modifier = Modifier.fillMaxWidth()
