d0b1b8fc78932958f80742ef23979d397a685bf1
zohaib <zohaibansari100@gmail.com>
2021-04-11 17:25:54 +0530

    implement searching

--- ComposeNotesDesktop/src/main/kotlin/main.kt
+++ ComposeNotesDesktop/src/main/kotlin/main.kt
@@ -19,28 +19,6 @@ import ui.notesList.NotesList
 @ExperimentalFoundationApi
 fun main() = Window {
 
-    val playerQueries: NoteQueries = DatabaseHelper.queries
-    playerQueries.insert(note_number = 1, title = "Corey Perry", body = "Hello !")
-    playerQueries.insert(
-        note_number = 2,
-        title = "Jetpack Compose",
-        body = "This is a declarative UI ramework developed by google and jetbrains"
-    )
-    playerQueries.insert(note_number = 4, title = "Perry the platypus", body = "Hello there perry !")
-    playerQueries.insert(
-        note_number = 5,
-        title = "Tyler Blevins NINJA",
-        body = "Professional fortnite player and streamer on twitch.tv/ninja "
-    )
-    playerQueries.insert(note_number = 6, title = "Corey Perry", body = "Hello !")
-    playerQueries.insert(
-        note_number = 7,
-        title = "Jetpack Compose",
-        body = "This is a declarative UI ramework developed by google and jetbrains"
-    )
-    playerQueries.insert(note_number = 8, title = "Perry the platypus", body = "Hello there perry !")
-
-    //val viewModel = NoteViewModel()
     var screenState by remember { mutableStateOf<Configuration>(Configuration.Screen1) }
 
     MaterialTheme {
--- ComposeNotesDesktop/src/main/kotlin/ui/notesList/NotesList.kt
+++ ComposeNotesDesktop/src/main/kotlin/ui/notesList/NotesList.kt
@@ -3,7 +3,6 @@ package ui.notesList
 import DatabaseHelper
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
@@ -12,7 +11,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Add
-import androidx.compose.material.icons.sharp.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.zohaib.ComposeNotesDesktop.model.Note
@@ -42,19 +39,21 @@ fun NotesList(onItemClick: (Note) -> Unit) {
         topBar = {
             Row(
                 modifier = Modifier.fillMaxWidth()
-                    .height(70.dp)
                     .background(purpleD1)
             ) {
                 TextField(
                     modifier = Modifier
-                        .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 5.dp)
+                        .padding(start = 20.dp, end = 20.dp, top = 10.dp)
                         .fillMaxWidth()
-                        .height(60.dp)
                         .clip(RoundedCornerShape(5.dp))
                         .background(purpleD3),
                     textStyle = TextStyle(
                         color = Color.White,
                         fontSize = 15.sp,
+                    ),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        cursorColor = Color.Gray,
+                        focusedBorderColor = Color.Gray
                     ),
                     singleLine = true,
                     value = searchQuery.value,
@@ -90,86 +89,11 @@ fun NotesList(onItemClick: (Note) -> Unit) {
             modifier = Modifier.fillMaxSize().background(purpleD1)
         ) {
             itemsIndexed(
-                items = noteList
-            ) { _, note ->
+                items = noteList.filter { it.title.contains(searchQuery.value, ignoreCase = true) }
+            ) { _ , note ->
                 NotesListItem(note, onItemClick)
             }
         }
     }
 
 }
-
-//@Composable
-//fun NotesListTopBar(vm: MainViewModel, searchParam: String) {
-//
-//    val isSearchBarVisible by vm.searchViewVisible.observeAsState(false)
-//
-//    Crossfade(targetState = isSearchBarVisible) { isVisible ->
-//        when (isVisible) {
-//            true -> {
-//                Row(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .height(70.dp)
-//                ) {
-//                    TextField(
-//                        placeholder = { Text(text = "Enter Search Query") },
-//                        singleLine = true,
-//                        textStyle = TextStyle(fontSize = 14.sp),
-//                        modifier = Modifier
-//                            .fillMaxWidth(.9f)
-//                            .padding(start = 8.dp),
-//                        value = searchParam, onValueChange = {
-//                            vm.updateSearchParam(it)
-//                        },
-//                        colors = TextFieldDefaults.outlinedTextFieldColors(
-//                            backgroundColor = purpleD5
-//                        )
-//                    )
-//                    Icon(
-//                        imageVector = Icons.Sharp.Clear,
-//                        tint = purpleD1,
-//                        modifier = Modifier
-//                            .weight(1f)
-//                            .wrapContentWidth(Alignment.End)
-//                            .padding(end = 6.dp, top = 16.dp)
-//                            .size(30.dp)
-//                            .clickable {
-//                                vm.searchViewVisibility(false)
-//                                vm.updateSearchParam("")
-//                            }
-//                    )
-//                }
-//
-//            }
-//            false -> {
-//                Row(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .height(70.dp)
-//                ) {
-//                    Spacer(modifier = Modifier.width(4.dp))
-//                    Text(
-//                        modifier = Modifier.padding(8.dp),
-//                        text = "📝 Notes",
-//                        style = robotoCus.subtitle2,
-//                        fontSize = 24.sp,
-//                    )
-//                    Icon(
-//                        imageVector = Icons.Sharp.Search,
-//                        tint = purpleD1,
-//                        modifier = Modifier
-//                            .weight(1f)
-//                            .wrapContentWidth(Alignment.End)
-//                            .padding(top = 16.dp, end = 8.dp)
-//                            .size(30.dp)
-//                            .clickable {
-//                                vm.searchViewVisibility(true)
-//                            }
-//                    )
-//                }
-//            }
-//        }
-//
-//    }
-//}
